bc0b087a2743f2205c47dae2e2d8f61eb41659be
grejfla <grejfla@gmail.com>
2026-01-16 20:41:07 -0300

    PROJETO_JAVA_SCRIPT| Flavio: criação do Index e código com variáveis

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var nome_pessoa, idade_pessoa, altura_pessoa, sexo;
+nome_pessoa= "Flávio";
+idade_pessoa= 53;
+altura_pessoa= 1.83;
+sexo = "masculino";
+
+console.log("Olá mundo. meu nome é "+ nome_pessoa +  " e tenho "  + idade_pessoa + " minha altura é " + altura_pessoa + " sou do sexo " +  sexo);
+
+
+
