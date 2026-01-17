07f23cd21e7537e60c473725cdd7d2c969668b51
grejfla <grejfla@gmail.com>
2026-01-16 21:29:36 -0300

    PROJETO_JAVA-SCRIPT: Flavio adição de novas variáveis e funcionalidades

--- javascript.js
+++ javascript.js
@@ -4,7 +4,30 @@ idade_pessoa= 53;
 altura_pessoa= 1.83;
 sexo = "masculino";
 
-console.log("Olá mundo. meu nome é "+ nome_pessoa +  " e tenho "  + idade_pessoa + " minha altura é " + altura_pessoa + " sou do sexo " +  sexo);
+console.log("Olá mundo. meu nome é "+ nome_pessoa +  " e tenho "  + idade_pessoa + " anos de idade " + " minha altura é "  + altura_pessoa + " sou do sexo " +  sexo);
+
+var  num1, num2, soma, subtracao, multiplicacao, divisao;
+num1 = 4;
+num2 = 3;
+
+soma = num1+num2;
+subtracao = num1-num2;
+multiplicacao = num1*num2;
+divisao = num1/num2;
+console.log("A soma é: "+soma);
+console.log("A subtração é: "+subtracao);
+console.log("A multiplicacao é: "+multiplicacao);
+console.log("A divisao é: "+divisao);
+
+var idade;
+idade=53;
+if(idade >=18){
+idade = "maior de idade";
+}
+else {
+    idade= "menor de idade";
+}
+console.log( "a pessoa é " +  idade);
 
 
 
